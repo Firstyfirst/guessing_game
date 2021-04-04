612c030f4e6ae214bdd7ed50a4cc6730f9da33b0
Sorawichaya Tiratrakoonwichaya <tsorawichaya@gmail.com>
2021-04-04 22:07:22 +0700

    fix find_one bug

--- app/sample.py
+++ app/sample.py
@@ -12,56 +12,10 @@ db = mongoClient[os.environ['MONGODB_DATABASE']]
 # connect to Redis
 redisClient = redis.Redis(host=os.environ.get("REDIS_HOST", "localhost"), port=os.environ.get("REDIS_PORT", 6379), db=os.environ.get("REDIS_DB", 0))
 
-@application.route('/', methods=['GET', 'POST'])
-def index():
-    
-    games = db.game.find_one({"stage":1})
+@application.route('/')
+def start():
     body = '<div style="text-align:center;">'
-    if games != None:
-        
-        body += '<h1>Game not ready</h1>'
-        # Initialize the document in database
-        step = games['step']
-        questions = games['question']
-        question = ",".join(questions)
-        guessing = ",".join(games['guessing'])
-        answer = ",".join(games['answer'])
-        fail = games['fail']
-        ans_index = len(games['answer'])
-        game_step = ['First', 'Second', 'Thrid', 'Fourth']
-
-        # Question Phase (step 0 --> 3)
-        if step < 4:
-            body += f"<h1>Choose A, B, C or D to add the {game_step[step]} character to the question</h1>"
-            body += f"<h2>{question}</h2><br></br>"
-            body += '<form action = "/q/" method = "post"> '
-            body += '<input type="submit" name="questioning" value="A" />'
-            body += '<input type="submit" name="questioning" value="B" /> '
-            body += '<input type="submit" name="questioning" value="C" /> '
-            body += '<input type="submit" name="questioning" value="D" /> '
-            body += '</form>'
-
-        # Answer Phase (step 4 --> 7)
-        elif step >= 4 and step < 8:
-            body += f"<h1>Choose A, B, C or D to guess the {game_step[step-4]} character</h1>"
-            body += f"<h2>{answer}</h2><br></br>"
-            body += f"<h2>Character(s) remaining: {guessing}</h2><br></br>"
-            body += '<form action = "/a/" method = "post">'
-            body += '<input type="submit" name="answering" value="A" />'
-            body += '<input type="submit" name="answering" value="B" /> '
-            body += '<input type="submit" name="answering" value="C" /> '
-            body += '<input type="submit" name="answering" value="D" /> '
-            body += '</form>'
-            body += f"<h2>Fail: {fail} <h2>"
-
-        # Game Over
-        elif step >= 8:
-            body += f"<h1> Game Over: You Wins!!</h1>"
-            body += f"<h2>The answer is {answer}</h2>"
-            body += '<a href ="/f/" class = "btn btn-primary" type = "button"> Retry </a>'
-
-    else:
-        db.game.insert_one({'stage' : 1,
+    db.game.insert_one({'id' : 1,
                             'question' : ["_","_","_","_"],
                             'guessing' : ["*","*","*","*"],
                             'answer' : [],
@@ -69,36 +23,85 @@ def index():
                             'fail' : 0,
                             'step' : 0,
                             })
-        body += '<h1>Guessing Game</h1>'
-        body += '<a href="/" class = "btn btn-primary" type = "button"> Start </a>'
+    body += '<h1>Guessing Game</h1>'
+    body += '<a href="/index" class = "btn btn-primary" type = "button"> Start </a>'
+    body += '</div>'
+    return body
+
+@application.route('/index', methods=['GET', 'POST'])
+def index():
+    
+    games = db.game.find_one({"id":1})
+    body = '<div style="text-align:center;">'
+    
+    body += '<h1>Game not ready</h1>'
+    # Initialize the document in database
+    step = games['step']
+    questions = games['question']
+    question = ",".join(questions)
+    guessing = ",".join(games['guessing'])
+    answer = ",".join(games['answer'])
+    fail = games['fail']
+    ans_index = len(games['answer'])
+    game_step = ['First', 'Second', 'Thrid', 'Fourth']
+
+    # Question Phase (step 0 --> 3)
+    if step < 4:
+        body += f"<h1>Choose A, B, C or D to add the {game_step[step]} character to the question</h1>"
+        body += f"<h2>{question}</h2><br></br>"
+        body += '<form action = "/q/" method = "post"> '
+        body += '<input type="submit" name="questioning" value="A" />'
+        body += '<input type="submit" name="questioning" value="B" /> '
+        body += '<input type="submit" name="questioning" value="C" /> '
+        body += '<input type="submit" name="questioning" value="D" /> '
+        body += '</form>'
+
+    # Answer Phase (step 4 --> 7)
+    elif step >= 4 and step < 8:
+        body += f"<h1>Choose A, B, C or D to guess the {game_step[step-4]} character</h1>"
+        body += f"<h2>{answer}</h2><br></br>"
+        body += f"<h2>Character(s) remaining: {guessing}</h2><br></br>"
+        body += '<form action = "/a/" method = "post">'
+        body += '<input type="submit" name="answering" value="A" />'
+        body += '<input type="submit" name="answering" value="B" /> '
+        body += '<input type="submit" name="answering" value="C" /> '
+        body += '<input type="submit" name="answering" value="D" /> '
+        body += '</form>'
+        body += f"<h2>Fail: {fail} <h2>"
+
+    # Game Over
+    elif step >= 8:
+        body += f"<h1> Game Over: You Wins!!</h1>"
+        body += f"<h2>The answer is {answer}</h2>"
+        body += '<a href ="/f/" class = "btn btn-primary" type = "button"> Retry </a>'
     body += '</div>'
     return body
 
 @application.route('/q/', methods=['POST'])
 def questioning():
     ques = request.form.get('questioning')
-    db.game.update_one({'stage' : 1}, {"$inc" : {"step" : 1}})
+    db.game.update_one({'id' : 1}, {"$inc" : {"step" : 1}})
     db.game.update_one({'question': '_'}, {'$set': {'question.$': ques}})
     return index()
 
 @application.route('/a/', methods=['POST'])
 def answering():
     ans = request.form.get('answering')
-    games = db.game.find_one({"stage":1})
+    games = db.game.find_one({"id":1})
     questions = games['question']
     ans_index = len(games['answer'])
     if questions[ans_index] == ans:
-        db.game.update_one({'stage': 1}, {"$push": {"answer": ans}})
-        db.game.update_one({'stage': 1}, {"$inc": {"step": 1}})
-        db.game.update_one({'stage': 1}, {"$pop": {"guessing": 1}})
+        db.game.update_one({'id': 1}, {"$push": {"answer": ans}})
+        db.game.update_one({'id': 1}, {"$inc": {"step": 1}})
+        db.game.update_one({'id': 1}, {"$pop": {"guessing": 1}})
     else:
-        db.game.update_one({'stage': 1}, {"$inc": {"fail": 1}})
+        db.game.update_one({'id': 1}, {"$inc": {"fail": 1}})
     return index()
 
 @application.route('/f/')
 def finish():
     db.game.remove({})
-    return index()
+    return start()
 
 if __name__ == "__main__":
     ENVIRONMENT_DEBUG = os.environ.get("FLASK_DEBUG", True)
